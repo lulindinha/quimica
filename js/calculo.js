68433f7bc22beec306bc05d0bcbf9cf31dca7cde
Luísa Marcolin <luisamarcolin10@gmail.com>
2019-11-16 22:52:59 -0300

    Adicionado template

---
+++ js/calculo.js
@@ -0,0 +1,36 @@
+function concentracao_molar(){
+	mols_soluto = parseFloat(document.getElementById('mols_soluto').value);
+	volume_solucao = parseFloat(document.getElementById('volume_solucao').value);
+	// alert(mols_soluto/volume_solucao);
+	document.getElementById('result').innerHTML = mols_soluto/volume_solucao;
+	document.getElementById("popup").style.visibility = "visible";
+}
+
+function concentracao_comum(){
+	massa_soluto = parseFloat(document.getElementById('massa_soluto').value);
+	volume_solucao_comum = parseFloat(document.getElementById('volume_solucao_comum').value);
+	// alert(massa_soluto/volume_solucao_comum);
+	document.getElementById('result').innerHTML = massa_soluto/volume_solucao_comum;
+	document.getElementById("popup").style.visibility = "visible";
+}	
+
+
+function diluicao(){
+	concentracao_inicial = parseFloat(document.getElementById('concentracao_inicial').value);
+	concentracao_final = parseFloat(document.getElementById('concentracao_final').value);
+	volume_inicial = parseFloat(document.getElementById('volume_inicial').value);
+	volume_final = parseFloat(document.getElementById('volume_final').value);
+
+	if isNaN(concentracao_inicial){
+		var1 = concentracao_final * volume_final;
+		result = var1/volume_inicial;
+	}
+
+	// alert(massa_soluto_diluicao/volume_solucao_diluicao);
+	document.getElementById('result').innerHTML = result;
+	document.getElementById("popup").style.visibility = "visible";
+}	
+
+function fechar(){
+	document.getElementById("popup").style.visibility = "hidden";
+}
