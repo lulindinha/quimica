48e9a5d880cf8b58491a096561da9ca5bd5449df
Morgana Oliveira <morgana2_oliveira@hotmail.com>
2019-11-17 17:34:41 -0300

    conversão de unidades adicionado

---
+++ js/Conversão_de_unidades.js
@@ -0,0 +1,39 @@
+function conversão_unidade_massa(){
+	valor_massa = parseFloat(document.getElementById('valor_massa').value);
+	var unidade = document.getElementById("massa_unidade");
+	if (massa_unidade.options[massa_unidade.selectedIndex].value == 'kg') {
+		calculo = (valor_massa * 1000);
+	} if (massa_unidade.options[massa_unidade.selectedIndex].value == 'hg'){
+		calculo = (valor_massa * 100);
+	} if (massa_unidade.options[massa_unidade.selectedIndex].value == 'dag'){
+		calculo = (valor_massa * 10);
+	} if (massa_unidade.options[massa_unidade.selectedIndex].value == 'cg'){
+		calculo = (valor_massa / 100);
+	} if (massa_unidade.options[massa_unidade.selectedIndex].value == 'mg'){
+		calculo = (valor_massa / 1000);
+	}
+	document.getElementById('result').innerHTML = calculo;
+	document.getElementById("popup").style.visibility = "visible";
+}
+
+function conversão_unidade_volume(){
+	valor_volume = parseFloat(document.getElementById('valor_volume').value);
+	var unidade = document.getElementById("volume_unidade");
+	if (volume_unidade.options[volume_unidade.selectedIndex].value == 'kg') {
+		calculo = (valor_massa * 1000);
+	} if (volume_unidade.options[volume_unidade.selectedIndex].value == 'hg'){
+		calculo = (valor_massa * 100);
+	} if (volume_unidade.options[volume_unidade.selectedIndex].value == 'dag'){
+		calculo = (valor_massa * 10);
+	} if (volume_unidade.options[volume_unidade.selectedIndex].value == 'cg'){
+		calculo = (valor_massa / 100);
+	} if (volume_unidade.options[volume_unidade.selectedIndex].value == 'mg'){
+		calculo = (valor_massa / 1000);
+	}
+	document.getElementById('result').innerHTML = calculo;
+	document.getElementById("popup").style.visibility = "visible";
+}
+
+function fechar(){
+	document.getElementById("popup").style.visibility = "hidden";
+}
