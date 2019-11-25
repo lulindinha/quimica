5477fd5729d26292c0fa7ae76508291b32d33545
Luísa Marcolin <luisamarcolin10@gmail.com>
2019-11-25 20:15:53 -0300

    JS do conversor arrumado

---
+++ js/Conversão_de_unidades.js
@@ -0,0 +1,57 @@
+// função para converter unidades de massa
+
+function conversao_unidade_massa(){
+	valor_massa = parseFloat(document.getElementById('valor_massa').value);
+	var massa_unidade = document.getElementById("massa_unidade");
+	
+	console.log(valor_massar);
+
+	if (massa_unidade.options[massa_unidade.selectedIndex].value == 'kg') {
+		calculo = (valor_massa * 1000);
+		document.getElementById('result').innerHTML = calculo;
+	} if (massa_unidade.options[massa_unidade.selectedIndex].value == 'hg'){
+		calculo = (valor_massa * 100);
+		document.getElementById('result').innerHTML = calculo;
+	} if (massa_unidade.options[massa_unidade.selectedIndex].value == 'dag'){
+		calculo = (valor_massa * 10);
+		document.getElementById('result').innerHTML = calculo;
+	} if (massa_unidade.options[massa_unidade.selectedIndex].value == 'cg'){
+		calculo = (valor_massa / 100);
+		document.getElementById('result').innerHTML = calculo;
+	} if (massa_unidade.options[massa_unidade.selectedIndex].value == 'mg'){
+		calculo = (valor_massa / 1000);
+		document.getElementById('result').innerHTML = calculo;
+	}
+
+	document.getElementById("popup").style.visibility = "visible";
+}
+
+// função para converter unidade de volume 
+
+function conversao_unidade_volume(){
+	valor_volume = parseFloat(document.getElementById('valor_volume').value);
+	var volume_unidade = document.getElementById("volume_unidade");
+	if (volume_unidade.options[volume_unidade.selectedIndex].value == 'hl') {
+		calculo = (valor_volume * 1000);
+		document.getElementById('result').innerHTML = calculo;
+	} if (volume_unidade.options[volume_unidade.selectedIndex].value == 'dl'){
+		calculo = (valor_volume * 100);
+		document.getElementById('result').innerHTML = calculo;
+	} if (volume_unidade.options[volume_unidade.selectedIndex].value == 'dal'){
+		calculo = (valor_volume * 10);
+		document.getElementById('result').innerHTML = calculo;
+	} if (volume_unidade.options[volume_unidade.selectedIndex].value == 'cl'){
+		calculo = (valor_volume / 100);
+		document.getElementById('result').innerHTML = calculo;
+	} if (volume_unidade.options[volume_unidade.selectedIndex].value == 'ml'){
+		calculo = (valor_volume / 1000);
+		document.getElementById('result').innerHTML = calculo;
+	}
+ 	
+	document.getElementById("popup").style.visibility = "visible";
+}
+
+function fechar(){
+	document.getElementById("popup").style.visibility = "hidden";
+}
+
